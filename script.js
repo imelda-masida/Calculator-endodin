55266f1d47aa04e4cfc5cbad6c48c33a2d70cfc7
stone <imeldamasidakakudji@gmail.com>
2026-07-30 11:54:11 +0200

    ajout fonction des console.log

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const add = function(nb1, nb2){
     return nb1 + nb2;
 };
+console.log(add);
+
+
 
 const soustra = function(nb1, nb2){
     return nb1 - nb2;
 };
+console.log(soustra);
+
+
 
 const multi = function(nb1, nb2){
     return nb1 * nb2;
 };
+console.log(multi);
+
+
+
+const division = function(nb1, nb2){
+    return nb1 / nb2;
+};
+console.log(division);
