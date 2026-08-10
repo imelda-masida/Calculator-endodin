4060958b48669571ec86e4383ece1c6d04c82383
stone <imeldamasidakakudji@gmail.com>
2026-08-10 22:09:57 +0200

    suppression de console.log et arrangement

--- script.js
+++ script.js
@@ -21,32 +21,26 @@ function addfenetre(resultat){
 const add = function(nb1, nb2){
     return nb1 + nb2;
 };
-console.log(add);
 
 
 
 const soustra = function(nb1, nb2){
     return nb1 - nb2;
 };
-console.log(soustra);
 
 
 
 const multi = function(nb1, nb2){
     return nb1 * nb2;
 };
-console.log(multi);
+
 
 
 
 const division = function(nb1, nb2){
     return nb1 / nb2;
 };
-console.log(division);
 
-let nb1 = 7;
-let nb2 = 2;
-let operateur = `-+/*`;
 
 function operation(op, nb1, nb2) {
     nb1 = parseFloat(nb1);
@@ -61,25 +55,5 @@ function operation(op, nb1, nb2) {
 }
 
 
-let nbActuel = "0";
-
-function metterAJourNbActuel(){
-    const ecran = document.getElementById("fenetre");
-    ecran.value = nbActuel;
-}
-
-function ajouterAuNbActuel(nb){
-    if (nbActuel === "0") {
-        nbActuel = nb;
-    } else {
-        nbActuel += nb;
-    }
-    
-}
-
-function cliquerBouton(nb){
-    ajouterAuNbActuel(nb);
-    metterAJourNbActuel();
-}
 
 
